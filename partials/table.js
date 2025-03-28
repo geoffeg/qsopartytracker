ce195d6e800e5a1a12c79cc873f0e427060fd261
Geoffrey Gallaway <geoffeg@geoffeg.org>
2025-03-28 11:44:10 -0500

    Change table refresh date to GMT

--- partials/table.js
+++ partials/table.js
@@ -57,7 +57,7 @@ const table = (positions) => {
     })
 
     const table = `
-    Refreshes every 30 seconds. Loaded: ${new Date().toLocaleString()}
+    Refreshes every 30 seconds. Loaded: ${new Date().toUTCString()}
     <table class="table">
         <thead>
             <tr>
